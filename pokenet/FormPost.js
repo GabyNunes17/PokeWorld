ab3bf0a34c5bceab50cc83ac5f6cfc9bcbc893d0
Verseck <rafaverseck@gmail.com>
2023-03-25 18:23:58 -0300

    ok

---
+++ pokenet/FormPost.js
@@ -0,0 +1,28 @@
+export class FormPost{
+  constructor(idForm, idTextarea, idULPost) {
+      this.Form = document.getElementById(idForm);
+      this.Textarea = document.getElementById(idTextarea);
+      this.ulpost = document.getElementById(idULPost);
+      this.addSubmit();
+ 
+    }
+
+
+    onSubmit(func){
+        this.form.addEventListener(`submit`,func)
+    }
+
+
+  addSubmit(){
+    const handleSubmit = (event) => {
+       event.preventDefault();
+       console.log(`Evento add.`)
+
+    }
+    this.onSubmit(handleSubmit)
+      
+ }
+  
+
+}
+const postform = new FormPost(`formpost`,`textarea`,`posts`)
