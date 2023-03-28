1ab9818ed56e80cf31a48d272a7642b66573f3f8
RAFAEL FERREIRA MACHADO <rafael.fmachado4@senacsp.edu.br>
2023-03-28 12:40:54 -0300

    Salvando comentario

--- pokenet/script.js
+++ pokenet/script.js
@@ -3,3 +3,13 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+ function mostrarComentario() {
+  const form = document.querySelector("#formulario");
+
+  form.addEventListener("click", (event) => {
+    event.preventDefault();
+    let coment = document.querySelector("#comentario").value;
+    alert(coment)
+  });
+ }
